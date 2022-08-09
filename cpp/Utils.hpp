10a12326f8e580d8d519f3809e48856e884ad0db
binhnguyen288-source <nguyenthebinh288@gmail.com>
2022-08-09 19:07:06 +0700

    pure equirectangular to cubemap

--- cpp/Utils.hpp
+++ cpp/Utils.hpp
@@ -6,26 +6,18 @@ struct RGBA {
     std::uint8_t* data;
     int width, height;
 
-    RGBA(int width, int height) : 
-        data{new std::uint8_t[4 * width * height]}, 
-        width{width}, height{height} {}
+    RGBA(std::uint8_t* data, int width, int height) : 
+        data{data}, width{width}, height{height} {}
     
     RGBA(RGBA&& other) = delete;
     RGBA(RGBA const& other) = delete;
     RGBA& operator=(RGBA const& other) = delete;
     RGBA& operator=(RGBA&& other) = delete;
-
-    ~RGBA() { delete[] data; }
+    ~RGBA() = default;
 
     std::uint8_t const* getCPixelWrap(int row, int col) const {
         row  = std::clamp(row, 0, height - 1);
         col %= width;
-        return &data[4 * (row * width + col)];
-    }
-
-    std::uint8_t const* getCPixelRaw(int row, int col) const {
-        row = std::clamp(row, 0, height - 1);
-        col = std::clamp(col, 0, width - 1);
         return &data[4 * (row * width + col)];
     }
     
@@ -51,7 +43,7 @@ enum Face {
 };
 
 template<Face face>
-Vec3D mapsToSpherical(float a, float b) {
+inline Vec3D mapsToSpherical(float a, float b) {
     if constexpr (face == LEFT)
         return {1, a, b};
     if constexpr (face == FRONT)
@@ -66,36 +58,6 @@ Vec3D mapsToSpherical(float a, float b) {
         return {a, b, -1};
 }
 
-
-
-
-inline std::pair<float, float> mapsToCube(float x, float y, float z) {
-
-
-    float offset = 1;
-
-    if (std::fabs(x) < std::fabs(y)) {
-        std::swap(x, y);
-        offset = 3;
-    }
-
-    if (std::fabs(x) < std::fabs(z)) {
-        std::swap(x, z);
-        if (offset < 2) std::swap(y, z);
-        offset = 5;
-    }
-
-    float const norm = 1.0f / std::fabs(x);
-
-    x *= norm;
-    y *= norm;
-    z *= norm;
-    
-    return {
-        (1 + y) / 2,
-        offset + (z - x) / 2,
-    };
-}
 template<Face face>
 void toCubeMapFace(RGBA const& src, RGBA& dst) {
     const int nCubeSide = src.width / 2;
@@ -133,17 +95,4 @@ void toCubeMapFace(RGBA const& src, RGBA& dst) {
         }
     }
 
-}
-
-RGBA* toCubeMap(RGBA const& src) {
-    
-    int nCubeSide = src.width / 2;
-    RGBA* dst = new RGBA(nCubeSide, 6 * nCubeSide);
-    toCubeMapFace<LEFT>(src, *dst);
-    toCubeMapFace<FRONT>(src, *dst);
-    toCubeMapFace<RIGHT>(src, *dst);
-    toCubeMapFace<BACK>(src, *dst);
-    toCubeMapFace<TOP>(src, *dst);
-    toCubeMapFace<BOTTOM>(src, *dst);
-    return dst;
 }
